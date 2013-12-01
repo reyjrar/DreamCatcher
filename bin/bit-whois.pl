721ab2a96b0b62839983809b2bf4b3dd838c059d
Brad Lhotsky <brad@divisionbyzero.net>
2013-12-01 22:32:26 +0100

    Better output for the bit-whois.pl, including list output of available domains for easy bulk registration

--- bin/bit-whois.pl
+++ bin/bit-whois.pl
@@ -19,7 +19,7 @@ $domain ||= 'google.com';
 my @parts = split /\./, lc $domain;
 
 my $word = shift @parts;
-my @variations = ();
+my @variations = ( $domain );
 
 for my $place ( 0 .. length($word)-1 ) {
     my $letter = substr($word,$place,1);
@@ -36,8 +36,8 @@ for my $place ( 0 .. length($word)-1 ) {
     }
 }
 
+my @available = ();
 foreach my $variation (@variations) {
-
     my ($raw,$info) = (undef,'');
     eval {
         $raw = whois($variation);
@@ -54,10 +54,10 @@ foreach my $variation (@variations) {
             die "parse error" unless defined $result && ref $result eq 'HASH';
 
             if( exists $result->{nameservers} ) {
-                $info = exists $result->{nameservers}[0]->{domain} ? $result->{nameservers}[0]->{domain} : $result->{nameservers}[0]->{ip};
+                $info = join (',', sort map { exists $_->{domain} ? $_->{domain} : $_->{ip}  } @{ $result->{nameservers} } );
             }
             elsif(exists $result->{emails} ) {
-                $info = $result->{emails}[0];
+                $info = join( ',', sort @{ $result->{emails} } );
             }
             else {
                 foreach my $f (qw(admin_email tech_email billing_email)) {
@@ -69,9 +69,25 @@ foreach my $variation (@variations) {
         $error .= "\n$@" if $@;
     }
 
-    printf "$domain variation $variation is %s\n", defined $info ? "taken ($info)" :
-                                                   defined $error  ? '!! ERROR !!' : '** AVAILABLE **';
-    print "(error) $error\n" if defined $error && length $error;
-    print map { "   $_\n" } split /[\r\n]+/, $raw if !$info && defined $error && defined $raw && length $raw;
-}
+    if( $domain eq $variation ) {
+        # Reference:
+        if( defined $info ) {
+            print "# Reference $domain found with $info\n";
+        }
+        else {
+            print "# Reference $domain NOT FOUND\n";
+        }
+    }
+    else {
 
+        printf "$domain variation $variation is %s\n", defined $info ? "taken ($info)" :
+                                                    defined $error  ? '!! ERROR !!' : '** AVAILABLE **';
+        print "(error) $error\n" if defined $error && length $error;
+        print map { "   $_\n" } split /[\r\n]+/, $raw if !$info && defined $error && defined $raw && length $raw;
+        push @available, $variation if !defined $info && !defined $error;
+    }
+}
+if( @available ) {
+    print "\n # Available Variations on $domain\n\n";
+    print "$_\n" for @available;
+}
