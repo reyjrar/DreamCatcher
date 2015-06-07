dcc10f89faf0dc8b58b295320b8c03868f7c2b6a
Brad Lhotsky <brad.lhotsky@booking.com>
2015-06-07 05:39:56 -0600

    Add the since_ts field to list_tracking_client.

--- sql/schema/install/plugins/list/list_tracking_client.sql
+++ sql/schema/install/plugins/list/list_tracking_client.sql
@@ -6,6 +6,7 @@ CREATE TABLE list_tracking_client
   reference_count integer DEFAULT 0,
   first_ts timestamp without time zone NOT NULL DEFAULT now(),
   last_ts timestamp without time zone NOT NULL DEFAULT now(),
+  since_ts timestamp without time zone NOT NULL DEFAULT now(),
   CONSTRAINT list_tracking_client_pkey PRIMARY KEY (list_id, client_id),
   CONSTRAINT list_tracking_client_fki_list FOREIGN KEY (list_id)
       REFERENCES list (id) MATCH SIMPLE
