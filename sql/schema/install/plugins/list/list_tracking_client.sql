70d35dd5a4afc170ab6939e25b17081df811180c
Brad Lhotsky <brad.lhotsky@booking.com>
2015-06-07 05:31:45 -0600

    Add the list_tracking_client() function to handle the list::tracking analysis plugin.

--- sql/schema/install/plugins/list/list_tracking_client.sql
+++ sql/schema/install/plugins/list/list_tracking_client.sql
@@ -17,3 +17,30 @@ CREATE TABLE list_tracking_client
 WITH (
   OIDS=FALSE
 );
+
+CREATE OR REPLACE FUNCTION list_tracking_client(
+    in_list_id integer,
+    in_client_id integer,
+    in_first_ts timestamp without time zone,
+    in_last_ts timestamp without time zone,
+    in_reference_count integer)
+  RETURNS void AS
+$BODY$
+BEGIN
+    PERFORM 1 FROM list_tracking_client WHERE list_id = in_list_id AND client_id = in_client_id;
+    IF NOT FOUND THEN
+        INSERT INTO list_tracking_client ( list_id, client_id, first_ts, last_ts, reference_count, since_ts )
+            VALUES ( in_list_id, in_client_id, in_first_ts, in_last_ts, in_reference_count, in_first_ts );
+    ELSE
+    UPDATE list_tracking_client
+        SET reference_count = in_reference_count,
+            first_ts = (CASE WHEN in_first_ts < first_ts THEN in_first_ts ELSE first_ts END),
+            last_ts = (CASE WHEN in_last_ts > last_ts THEN in_last_ts ELSE last_ts END),
+            since_ts = in_first_ts
+        WHERE list_id = in_list_id AND client_id = in_client_id;
+    END IF;
+
+    RETURN;
+END$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
