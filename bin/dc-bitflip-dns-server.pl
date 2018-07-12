ac87d0a506e080f289b2ca710c8774f56fbf049d
Brad Lhotsky <brad@divisionbyzero.net>
2018-07-11 22:10:40 -0700

    Full fledged bitflip DNS server
    Implement the bitflipping responses correctly.  Since the corruption of
    the data happens elsewhere, the resolver library is expecting a response
    that matches the unflipped domain.

--- bin/dc-bitflip-dns-server.pl
+++ bin/dc-bitflip-dns-server.pl
@@ -3,47 +3,112 @@
 use strict;
 use warnings;
 
+use CLI::Helpers qw(:output);
+use Getopt::Long::Descriptive;
+use JSON::MaybeXS;
 use Net::DNS;
 use Net::DNS::Nameserver;
-use Sys::Syslog;
+use YAML;
 
-my $SquattedDomains = join '|', map { quotemeta } qw(
-    bnoking.com
-    bookiog.com
+my %DEFAULT = (
+    port => 53,
+    addr => "0.0.0.0",
 );
-my $MATCH = qr/($SquattedDomains)/;
-my %ANSWER = (
-    A => '96.126.104.52',
-    MX => '10 mx.db0.us',
+my ($opt,$usage) = describe_options("%c %o",
+    ['config=s', "Config file", { required => 1 } ],
+    [],
+    ['addr=s',  "DNS Listening Address, default $DEFAULT{addr}",
+                { default => $DEFAULT{addr} }],
+    ['port=i',  "DNS Listening Port, default $DEFAULT{port}",
+                { default => $DEFAULT{port} }],
+    [],
+    ['help', "Display this help and exit", { shortcircuit => 1 }],
 );
+if( $opt->help ) {
+    print $usage->text;
+    exit;
+}
 
-openlog("bitsquatting", "ndelay", "local0");
+=head2 CONFIG File Format
+
+The YAML config file looks like this:
+
+    ---
+    domains:
+      booking.com:
+        records:
+          A: 1.2.3.4
+          MX: 10 mx.example.com
+        flipped:
+          - bnoking.com
+          - bookiog.com
+
+=cut
+
+my $CFG = YAML::LoadFile($opt->config);
+
+my %DomainMap = ();
+foreach my $domain ( sort keys %{ $CFG->{domains} } ) {
+    foreach my $flipped ( @{ $CFG->{domains}{$domain}{flipped} } ) {
+        $DomainMap{$flipped} = $domain;
+    }
+}
+
+my $MATCH = join( '|', map { quotemeta } sort keys %DomainMap );
+my $JSON = JSON->new->canonical->utf8;
 
 sub handle_request {
     my @incoming = @_;
     my @names    = qw(qname qclass qtype peerhost query conn);
     my %q = map { shift(@names) => $_ } @incoming;
 
-    my $message = sprintf "QUERY: %s %s %s from %s\n", @q{qw(qclass qtype qname peerhost)};
-    syslog("info", $message);
+    my $log = {
+        src_ip => $q{peerhost},
+        query  => {
+            class => $q{qclass},
+            type => $q{qtype},
+            name => $q{qname},
+        },
+    };
 
-    my $rcode = "NXDOMAIN";
+    my $rcode = $log->{status} = "NXDOMAIN";
     my (@ans,@auth,@add);
 
     my $name = lc $q{qname};
-    if( exists $ANSWER{$q{qtype}} && $name =~ /$MATCH/ ) {
-        $rcode = "NOERROR";
-        push @ans, new Net::DNS::RR("$q{qname} 5 $q{qclass} $q{qtype} $ANSWER{$q{qtype}}");
+    if( my($flipped) = ($name =~ /($MATCH)$/) ) {
+        $log->{src_domain} = $flipped;
+        if( my $domain = $DomainMap{$flipped} ) {
+            $log->{dst_domain} = $domain;
+            if( exists $CFG->{domains}{$domain}{records}{$q{qtype}} ) {
+                $rcode = $log->{status} = "NOERROR";
+                my $answer = $CFG->{domains}{$domain}{records}{$q{qtype}};
+                my $faked  = $q{qname} =~ s/$flipped/$domain/ri;
+                # Faked Response
+                push @ans, Net::DNS::RR->new("$faked $q{qclass} $q{qtype} $answer");
+                # Original
+                push @add, Net::DNS::RR->new("$q{qname} $q{qclass} $q{qtype} $answer");
+            }
+        }
     }
+    output($log->{status} eq 'NOERROR' ? {color=>"green"} :{stderr=>1,color=>'red'},
+        sprintf "QUERY %s [%s] from %s for '%s %s %s': %s",
+            $log->{status},
+            $log->{src_domain} || 'UNKNOWN',
+            $log->{src_ip},
+            $log->{query}{class},
+            $log->{query}{type},
+            $log->{query}{name},
+            $JSON->encode($log)
+    );
 
     return ( $rcode, \@ans, \@auth, \@add, { aa => 1 } );
 }
 
 my $ns = Net::DNS::Nameserver->new(
-    LocalAddr => '0.0.0.0',
-    LocalPort => 53,
+    LocalAddr => $opt->addr,
+    LocalPort => $opt->port,
     ReplyHandler => \&handle_request,
-    Verbose => 0,
+    Verbose => (CLI::Helpers::def('VERBOSE') || CLI::Helpers::def('DEBUG')) ? 1 : 0,
 );
-
+output({color=>'cyan'}, "bitflip dns server started");
 $ns->main_loop;
